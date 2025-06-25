eb4bf484c348fe4434ba1a71b8f70a35ff326926
Aweiro <arsenskott@gmail.com>
2025-06-25 16:50:30 +0300

    make build

---
+++ index.8ee1a0dd.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=0;l<e.rows.length;l++){var n=e.rows[l].cells[1].innerHTML,r=e.rows[l],t=document.createElement(e.rows[l].cells[1].tagName),c=r.cells[4].innerHTML;t.textContent=n,r.appendChild(t),r.cells[4].innerHTML=r.cells[5].innerHTML,r.cells[5].innerHTML=c}
+//# sourceMappingURL=index.8ee1a0dd.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const table = document.querySelector("table");
+for(let i = 0; i < table.rows.length; i++){
+    const row = table.rows[i];
+    const rowCells = row.cells;
+    const td = document.createElement(rowCells[1].tagName);
+    const lastEl = rowCells[rowCells.length - 1];
+    lastEl.className = "lastEl";
+    td.textContent = rowCells[1].innerHTML;
+    row.querySelector(".lastEl").insertAdjacentElement("beforebegin", td);
+    lastEl.removeAttribute("class");
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
