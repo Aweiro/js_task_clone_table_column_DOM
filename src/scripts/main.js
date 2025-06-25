efc1a0f101e37c7185c7322f254bed6b202e27ea
Aweiro <arsenskott@gmail.com>
2025-06-25 16:04:06 +0300

    add task solution 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,12 @@ for (let i = 0; i < table.rows.length; i++) {
   const text = table.rows[i].cells[1].innerHTML;
   const row = table.rows[i];
   const td = document.createElement(table.rows[i].cells[1].tagName);
-  const reserv = row.cells[4].innerHTML;
+  const lastCell = table.rows[i].cells.length;
+  const reserv = row.cells[lastCell - 1].innerHTML;
 
   td.textContent = text;
   row.appendChild(td);
 
-  row.cells[4].innerHTML = row.cells[5].innerHTML;
-  row.cells[5].innerHTML = reserv;
+  row.cells[lastCell - 1].innerHTML = row.cells[lastCell].innerHTML;
+  row.cells[lastCell].innerHTML = reserv;
 }
