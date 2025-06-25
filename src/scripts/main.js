0d4dcd14f1f0e4730d4e29dfc68c25ca8bc36bd8
Aweiro <arsenskott@gmail.com>
2025-06-25 16:50:18 +0300

    add task solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,13 @@
 const table = document.querySelector('table');
 
 for (let i = 0; i < table.rows.length; i++) {
-  const text = table.rows[i].cells[1].innerHTML;
   const row = table.rows[i];
-  const td = document.createElement(table.rows[i].cells[1].tagName);
-  const lastCell = table.rows[i].cells.length;
-  const reserv = row.cells[lastCell - 1].innerHTML;
+  const rowCells = row.cells;
+  const td = document.createElement(rowCells[1].tagName);
+  const lastEl = rowCells[rowCells.length - 1];
 
-  td.textContent = text;
-  row.appendChild(td);
-
-  row.cells[lastCell - 1].innerHTML = row.cells[lastCell].innerHTML;
-  row.cells[lastCell].innerHTML = reserv;
+  lastEl.className = 'lastEl';
+  td.textContent = rowCells[1].innerHTML;
+  row.querySelector('.lastEl').insertAdjacentElement('beforebegin', td);
+  lastEl.removeAttribute('class');
 }
