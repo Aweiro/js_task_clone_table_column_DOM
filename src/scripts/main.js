f743483443b23d77e196f1dc3a17b343f27594f9
Aweiro <arsenskott@gmail.com>
2025-06-25 15:50:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const text = table.rows[i].cells[1].innerHTML;
+  const row = table.rows[i];
+  const td = document.createElement(table.rows[i].cells[1].tagName);
+  const reserv = row.cells[4].innerHTML;
+
+  td.textContent = text;
+  row.appendChild(td);
+
+  row.cells[4].innerHTML = row.cells[5].innerHTML;
+  row.cells[5].innerHTML = reserv;
+}
